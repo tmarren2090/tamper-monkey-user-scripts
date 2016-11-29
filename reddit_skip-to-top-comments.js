bce7455030f924be5519b9fbab0175eec681919b
Thomas Marren <tmarren2090@gmail.com>
2016-11-29 12:32:03 -0500

    click expando-buttons. need to click button again before leaving the comment to close it

--- reddit_skip-to-top-comments.js
+++ reddit_skip-to-top-comments.js
@@ -14,9 +14,9 @@
     var comments = document.getElementsByClassName('comment');
     var topComments = [];
     for (let i = 0; i < comments.length - 1; i++){
-      if(Array.from(comments[i].parentNode.classList).includes('nestedlisting')){
-        topComments.push(comments[i]);
-      }
+        if(Array.from(comments[i].parentNode.classList).includes('nestedlisting')){
+            topComments.push(comments[i]);
+        }
     }
     var nextComment = -1;
     var bgColor = '#F9F9E7';
@@ -26,21 +26,49 @@
     //var style = ['style', 'border-left: 3px solid red!important;'];
 
     document.addEventListener("keydown", function(e) {
-      if (nextComment != -1){ topComments[nextComment].removeAttribute(...style); }
-      // I use 'n' to go down and 'm' to go up
-      if(e.keyCode == 78){
-          e.preventDefault();
-          nextComment++;
-          topComments[nextComment].scrollIntoView();
-          topComments[nextComment].setAttribute(...style);
-      } else if (e.keyCode == 77 && nextComment > -1) {
-          e.preventDefault();
-          nextComment--;
-          if(nextComment != -1){
-              topComments[nextComment].scrollIntoView();
-              topComments[nextComment].setAttribute(...style);
-          }
-      }
+        if (nextComment != -1){ topComments[nextComment].removeAttribute(...style); }
+        // I use 'n' to go down and 'm' to go up
+        if(e.keyCode == 78){
+            e.preventDefault();
+            nextComment++;
+            topComments[nextComment].scrollIntoView();
+            topComments[nextComment].setAttribute(...style);
+            let expandoButtons = topComments[nextComment].children[2].children[1].children[1].children[0].getElementsByClassName('expando-button');
+            console.log(expandoButtons);
+            for (let i = 0; i < expandoButtons.length; i++){
+                let button = expandoButtons[i];
+                console.log(button);
+                if(!button.classList.contains('video')){
+                    eventFire(button, 'click');
+                }
+            }
+            console.log(nextComment);
+        } else if (e.keyCode == 77 && nextComment > -1) {
+            e.preventDefault();
+            nextComment--;
+            if(nextComment != -1){
+                topComments[nextComment].scrollIntoView();
+                topComments[nextComment].setAttribute(...style);
+                let expandoButtons = topComments[nextComment].children[2].children[1].children[1].children[0].getElementsByClassName('expando-button');
+                for (let i = 0; i < expandoButtons.length - 1; i++){
+                    let button = expandoButtons[i];
+                    if(!button.classList.contains('video')){
+                        eventFire(button, 'click');
+                    }
+                }
+            }
+        }
     });
+
+    //Simulates a click on any expando button in RES
+    function eventFire(el, etype){
+        if (el.fireEvent) {
+            el.fireEvent('on' + etype);
+        } else {
+            var evObj = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            el.dispatchEvent(evObj);
+        }
+    }
 
 })();
